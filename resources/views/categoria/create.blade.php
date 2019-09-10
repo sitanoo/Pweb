fe6410d29d8841baa461b49ff21878a668f2f1e3
Aluno <Aluno@SN-304591>
2019-09-09 21:24:21 -0300

    FEITO TABELA E ADD NO BANCO DE DADOS

---
+++ resources/views/categoria/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        
+    </head>
+    <body>
+        <form action="{{route('categoria.store')}}" method="post">
+            @csrf
+            
+            <label for="nomcat">Nome da categoria</label>
+            <input type="text" name="nomcat" id="nomcat"/><br/>
+            <button type="submit">Adicionar</button>
+        
+        </form>
+       
+    </body>
+</html>
